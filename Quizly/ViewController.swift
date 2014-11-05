90996c61c2c544c8645093419af5b2e1b35bc45f
Guy Halperin <guyhalperin@Guys-MacBook-Air.local>
2014-11-05 21:59:41 +0000

    Initial Commit

---
+++ Quizly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quizly
+//
+//  Created by Guy Halperin on 11/5/14.
+//  Copyright (c) 2014 Guy Halperin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
