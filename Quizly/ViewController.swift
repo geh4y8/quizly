0e1e0161e8e57b754a5d7923336cc06851e4b358
Guy Halperin <guyhalperin@Guys-MacBook-Air.local>
2014-11-05 22:39:37 +0000

    initial commit

--- Quizly/ViewController.swift
+++ Quizly/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var currentNumberOfPointsLabel: UILabel!
+    @IBOutlet weak var currentNumberOfLivesLabel: UILabel!
+    @IBOutlet weak var answerOneButton: UIButton!
+    @IBOutlet weak var answerTwoButton: UIButton!
+    @IBOutlet weak var answerThreeButton: UIButton!
+    @IBOutlet weak var answerFourButton: UIButton!
+    @IBOutlet weak var nextQuestionButton: UIButton!
+    @IBAction func answerOneButtonPressed(sender: UIButton) {
+    }
+    @IBAction func answerTwoButtonPressed(sender: UIButton) {
+    }
+    @IBAction func answerThreeButtonPressed(sender: UIButton) {
+    }
+    @IBAction func answerFourButtonPressed(sender: UIButton) {
+    }
+    @IBAction func nextQuestionButtonPressed(sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
